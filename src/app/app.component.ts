83a1bba159604c57e31294d5eb410ac1d04164fb
Mireau89 <marwen.aguerbi1989@gmail.com>
2019-07-16 14:46:15 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Post} from "./post";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-premier-angular-blog';
+
+  posts= [
+    new Post("Mon premier post",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    0),
+
+    new Post("Mon deuxième post",
+    "Nullam vehicula ipsum a arcu cursus. Non curabitur gravida arcu ac tortor dignissim convallis. " +
+    "Porttitor rhoncus dolor purus non. Nulla facilisi etiam dignissim diam quis enim lobortis. " +
+    "Commodo quis imperdiet massa tincidunt nunc pulvinar. Cras sed felis eget velit aliquet sagittis id consectetur purus.",
+    5),
+
+    new Post("Encore un post",
+    "Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. " +
+    "Proin fermentum leo vel orci porta non. Lectus urna duis convallis convallis tellus id interdum. " +
+    "Arcu risus quis varius quam quisque id diam. Varius morbi enim nunc faucibus a. " +
+    "Sit amet porttitor eget dolor morbi.",
+    -3)];
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { Post } from './post';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostListItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
